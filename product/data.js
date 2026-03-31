b7a570680c76f75ca2cf54aafaba7bfb84c2bb98
NewDawnPRG <vremenno44@gmail.com>
2026-03-31 23:12:24 +0300

    fix
    again

--- product/data.js
+++ product/data.js
@@ -1,19 +1,19 @@
 export const products = [
     {
       id: 1,
-      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4pRJnmBD3zch6OPcvmTL_tJrpYJ5ULpn6zA&s",
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSemR-zY8bvCpM11qQ9UUn5A7JOzRDSeTbeSg&s",
       title: "Ноутбук",
       text: "Мощный ноутбук для работы и игр"
     },
     {
       id: 2,
-      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJHPl0uDKmPeHCWCMscHqMuByGFelP2uTj9A&s",
+      src: "https://shop-connect.ru/upload/iblock/5ef/xyqkrp2wuvs2wywrv9929qo3garkkawo/403cfdcb83eb11f080daa8a159052af3_39c5bbda8a3911f080dca8a159052af3.png",
       title: "Смартфон",
       text: "Современный смартфон с отличной камерой"
     },
     {
       id: 3,
-      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVc9PYBPL62dq_HwuuwOj-C6Z5FliZHF9ETQ&s",
+      src: "https://mactime.pro/images/thumbnails/600/600/detailed/92/apple-airpods-max-2024-shining-star-1-600.jpg",
       title: "Наушники",
       text: "Беспроводные наушники с шумоподавлением"
     }
