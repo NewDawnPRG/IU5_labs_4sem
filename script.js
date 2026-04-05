04711f30f83f538a668d11c1768aa2b2f5292771
NewDawnPRG <vremenno44@gmail.com>
2026-04-06 00:29:28 +0300

    Did 3.6
    Ok im done

--- script.js
+++ script.js
@@ -28,10 +28,41 @@ function sumDiagonals(matrix) {
     return total;
 }
 
+function rle(str) {
+    if (str.length === 0) return "";
+
+    let result = "";
+    let count = 1;
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === str[i + 1]) {
+            count++;
+        } else {
+            result += str[i];
+            if (count > 1) result += count;
+            count = 1;
+        }
+    }
+
+    return result;
+}
+
+console.log("----------[TASK 1.1]----------");
+
 console.log(concatenate(['Я','Учусь','на','лучшей','кафедре'], ' '));
+
+console.log("----------[TASK 1.10]----------");
 
 const data = [0, 1, false, 2, undefined, '', 3, null];
 console.log(erase(data)) // [1, 2, 3]
 
+console.log("----------[TASK 2.7]----------");
+
 const matrix = [[1,2,3], [4,5,6], [7,8,9]];
 console.log(sumDiagonals(matrix));
+
+console.log("----------[TASK 3.6]----------");
+
+console.log(rle("aaabbbcc"));
+console.log(rle("abc"));
+console.log(rle(""));
