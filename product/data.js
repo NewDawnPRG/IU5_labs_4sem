aa68b056d286768edb1dcdae470fe3f3f45df123
NewDawnPRG <vremenno44@gmail.com>
2026-04-05 23:56:34 +0300

    Fix
    They have to be connected, why?...

--- product/data.js
+++ product/data.js
@@ -3,23 +3,23 @@ export const productStore = {
     {
       id: 1,
       src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkARIao6rosHFaBTy2SPnGl8Lpl_bAjQ8vhw&s",
-      title: "Яблоки",
-      text: "Сочные, сладкие, выращены в экологически чистых садах",
-      badge: "Органик"
+      title: "Продавец яблок",
+      text: "Зарплата 30.000-50.000 рублей",
+      badge: "Требуется сейчас"
     },
     {
       id: 2,
       src: "https://img.gazeta.ru/files3/102/21050102/ban3-pic_square_600x600-600x600-39432.jpg",
-      title: "Бананы",
-      text: "Спелые, богаты калием, идеальны для перекуса",
-      badge: "Скидка 20%"
+      title: "Продавец бананов",
+      text: "Зарплата 35.000-60.000 рублей",
+      badge: "Лучший выбор"
     },
     {
       id: 3,
       src: "https://apeti.ru/upload/iblock/8ae/nrow91y03tlkj6qauaqw8i4tlek18yd4/grusha_pakkham.png",
-      title: "Груши",
-      text: "Медовые, тают во рту, урожай 2027 года",
-      badge: "Новинка"
+      title: "Продавец груш",
+      text: "Зарплата 40.000-45.000 рублей",
+      badge: "Стабильность"
     }
   ],
   nextId: 4,
